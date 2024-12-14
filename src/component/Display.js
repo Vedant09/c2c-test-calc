23f642d06199ec708be07ece27dfe1b9e94d36cc
Vedant Yelsangikar <vedantvy98@gmail.com>
2024-12-14 01:14:24 -0500

    going back for test

--- src/component/Display.js
+++ src/component/Display.js
@@ -11,7 +11,6 @@ export default class Display extends React.Component {
   render() {
     return (
       <div className="component-display">
-        <h1>Vedant's Test</h1>
         <div>{this.props.value}</div>
       </div>
     );
