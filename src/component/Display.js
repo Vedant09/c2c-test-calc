e3b8f1471c2f4297ad2ce58027d12b8169c70b6e
Vedant Yelsangikar <vedantvy98@gmail.com>
2024-12-15 00:22:34 -0500

    Update Display.js

--- src/component/Display.js
+++ src/component/Display.js
@@ -11,7 +11,7 @@ export default class Display extends React.Component {
   render() {
     return (
       <div className="component-display">
-        <h3>ved</h3>
+        <h3>veda</h3>
         <div>{this.props.value}</div>
       </div>
     );
