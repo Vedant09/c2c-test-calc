37e3b3281f6b9f0a781b812b92ccaaf90cc71693
Vedant Yelsangikar <vedantvy98@gmail.com>
2024-12-14 23:03:03 -0500

    dsg

--- src/component/Display.js
+++ src/component/Display.js
@@ -11,7 +11,6 @@ export default class Display extends React.Component {
   render() {
     return (
       <div className="component-display">
-        <h4>ved</h4>
         <div>{this.props.value}</div>
       </div>
     );
